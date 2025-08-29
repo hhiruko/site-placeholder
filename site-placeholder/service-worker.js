1b8472b7427cdf252e7852c1c55f853285cc687e
hhiruko <hhiruko@users.noreply.github.com>
2025-08-29 10:49:53 +0000

    deploy: 5752f8b0946a07ee78263b55f8d078dcf5ae259b

---
+++ site-placeholder/service-worker.js
@@ -0,0 +1,127 @@
+
+    const assets = [
+  "/site-placeholder/index.html",
+  "/site-placeholder/img/smoki.PNG",
+  "/site-placeholder/img/misti.PNG",
+  "/site-placeholder/img/marta.PNG",
+  "/site-placeholder/favicons/yandex-browser-manifest.json",
+  "/site-placeholder/favicons/yandex-browser-50x50.png",
+  "/site-placeholder/favicons/mstile-70x70.png",
+  "/site-placeholder/favicons/mstile-310x310.png",
+  "/site-placeholder/favicons/mstile-310x150.png",
+  "/site-placeholder/favicons/mstile-150x150.png",
+  "/site-placeholder/favicons/mstile-144x144.png",
+  "/site-placeholder/favicons/manifest.webmanifest",
+  "/site-placeholder/favicons/icon.svg",
+  "/site-placeholder/favicons/favicon.ico",
+  "/site-placeholder/favicons/favicon-48x48.png",
+  "/site-placeholder/favicons/favicon-32x32.png",
+  "/site-placeholder/favicons/favicon-16x16.png",
+  "/site-placeholder/favicons/browserconfig.xml",
+  "/site-placeholder/favicons/apple-touch-startup-image-828x1792.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-750x1334.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-640x1136.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-2796x1290.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-2778x1284.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-2732x2048.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-2688x1242.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-2556x1179.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-2532x1170.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-2436x1125.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-2388x1668.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-2266x1488.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-2224x1668.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-2208x1242.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-2160x1640.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-2160x1620.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-2048x2732.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-2048x1536.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-1792x828.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-1668x2388.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-1668x2224.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-1640x2160.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-1620x2160.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-1536x2048.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-1488x2266.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-1334x750.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-1290x2796.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-1284x2778.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-1242x2688.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-1242x2208.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-1179x2556.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-1170x2532.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-1136x640.png",
+  "/site-placeholder/favicons/apple-touch-startup-image-1125x2436.png",
+  "/site-placeholder/favicons/apple-touch-icon.png",
+  "/site-placeholder/favicons/apple-touch-icon-precomposed.png",
+  "/site-placeholder/favicons/apple-touch-icon-76x76.png",
+  "/site-placeholder/favicons/apple-touch-icon-72x72.png",
+  "/site-placeholder/favicons/apple-touch-icon-60x60.png",
+  "/site-placeholder/favicons/apple-touch-icon-57x57.png",
+  "/site-placeholder/favicons/apple-touch-icon-180x180.png",
+  "/site-placeholder/favicons/apple-touch-icon-167x167.png",
+  "/site-placeholder/favicons/apple-touch-icon-152x152.png",
+  "/site-placeholder/favicons/apple-touch-icon-144x144.png",
+  "/site-placeholder/favicons/apple-touch-icon-120x120.png",
+  "/site-placeholder/favicons/apple-touch-icon-114x114.png",
+  "/site-placeholder/favicons/apple-touch-icon-1024x1024.png",
+  "/site-placeholder/favicons/android-chrome-96x96.png",
+  "/site-placeholder/favicons/android-chrome-72x72.png",
+  "/site-placeholder/favicons/android-chrome-512x512.png",
+  "/site-placeholder/favicons/android-chrome-48x48.png",
+  "/site-placeholder/favicons/android-chrome-384x384.png",
+  "/site-placeholder/favicons/android-chrome-36x36.png",
+  "/site-placeholder/favicons/android-chrome-256x256.png",
+  "/site-placeholder/favicons/android-chrome-192x192.png",
+  "/site-placeholder/favicons/android-chrome-144x144.png",
+  "/site-placeholder/cdn/water.system.min.css",
+  "/site-placeholder/cdn/water.light.min.css",
+  "/site-placeholder/cdn/water.dark.min.css",
+  "/site-placeholder/assets/index-iFxFCxMZ.css",
+  "/site-placeholder/assets/index-UlmPuTsi.js",
+  "/site-placeholder/"
+];
+    const CACHE_NAME = 'v2025-08-29T10:49:52.794Z';
+
+    self.addEventListener('install', event => {
+        self.skipWaiting();
+        event.waitUntil(
+            caches.open(CACHE_NAME).then(async cache => {
+                for(const asset of assets) {
+                    try {
+                        await cache.add(asset);
+                    } catch (e) {
+                        console.error('❌ Failed to cache:', asset, e);
+                    }
+                }
+            })
+        );
+    });
+
+    self.addEventListener('fetch', event => {
+        event.respondWith(
+            caches.match(event.request).then(response => {
+                if(response) return response;
+                return fetch(event.request).catch(() => {
+                    if(event.request.mode === 'navigate') {
+                        return caches.match('/site-placeholder/index.html');
+                    }
+                });
+            })
+        );
+    });
+
+    self.addEventListener('activate', event => {
+        self.clients.claim();
+        event.waitUntil(
+            caches.keys().then(cacheNames => {
+                return Promise.all(
+                    cacheNames.map(cacheName => {
+                        if (cacheName !== CACHE_NAME) {
+                            return caches.delete(cacheName);
+                        }
+                    })
+                );
+            })
+        );
+    });
